e313d7e2f8cf21358a46f93904358e37b4d153e7
Juuso Parkkinen <juuso.parkkinen@gmail.com>
2012-10-26 23:29:57 +0300

    Changed GetHRIaluejakokartat() to temporarily load the data from my dropbox

--- R/HRI.R
+++ R/HRI.R
@@ -25,32 +25,36 @@ GetHRIaluejakokartat <- function() {
   # Need to install package rgdal
   # Mac users, see http://www.r-bloggers.com/installing-rgdal-on-mac-os-x-2/
 
-  .InstallMarginal("rgdal")
-  .InstallMarginal("gpclib")
-  .InstallMarginal("ggplot2")
-
-  gpclibPermit()
-  
-  # Download KML files from http://www.hri.fi/fi/data/paakaupunkiseudun-aluejakokartat/
-  # Substitute manually "xsd:sting" in <SimpleField type="xsd:string" name="KOKOTUN"> and other similar fields with "string" to read whole metadata
-  pks.pienalue <- rgdal::readOGR(dsn="data/PKS_Kartta_Rajat_KML2011/PKS_pienalue2.kml", layer="pks_pienalue")
-  
-  pks.pienalue@data$id <- rownames(pks.pienalue@data) # Add IDs
-  pks.points <- ggplot2::fortify(pks.pienalue, region="id") # Get point data
-  
-  pks.points$group <- sub(".1", "", pks.points$group) # Regex PIEN to joinable format
-  
-  pks.df <- merge(pks.points, pks.pienalue@data, by.x="group", by.y = "id") # Put everything together
-  pks.df <- pks.df[order(pks.df$order),] # sort DF so that polygons come out in the right order
-  
-  # Fix encoding
-#  pks.df$Nimi <- factor(iconv(pks.df$Nimi, from="ISO-8859-1", to="UTF-8"))
-#  pks.df$NIMI_ISO <- factor(iconv(pks.df$NIMI_ISO, from="ISO-8859-1", to="UTF-8"))
-  pks.df$Name <- factor(iconv(pks.df$Name, from="ISO-8859-1", to="UTF-8"))
-#  pks.pienalue@data$Nimi <- factor(iconv(pks.pienalue@data$Nimi, from="ISO-8859-1", to="UTF-8"))
-#  pks.pienalue@data$NIMI_ISO <- factor(iconv(pks.pienalue@data$NIMI_ISO, from="ISO-8859-1", to="UTF-8"))
-  pks.pienalue@data$Name <- factor(iconv(pks.pienalue@data$Name, from="ISO-8859-1", to="UTF-8"))
-  
+  # NOTE! Changed to read data directly from my Dropbox, as the original code has strange problems - Juuso 26.10.2012
+  con <- url("http://dl.dropbox.com/u/792906/data/PKS_aluejakokartat_20111023.RData")
+  load(con)
+  close(con)
+#   .InstallMarginal("rgdal")
+#   .InstallMarginal("gpclib")
+#   .InstallMarginal("ggplot2")
+# 
+#   gpclibPermit()
+#   
+#   # Download KML files from http://www.hri.fi/fi/data/paakaupunkiseudun-aluejakokartat/
+#   # Substitute manually "xsd:sting" in <SimpleField type="xsd:string" name="KOKOTUN"> and other similar fields with "string" to read whole metadata
+#   pks.pienalue <- rgdal::readOGR(dsn="data/PKS_Kartta_Rajat_KML2011/PKS_pienalue2.kml", layer="pks_pienalue")
+#   
+#   pks.pienalue@data$id <- rownames(pks.pienalue@data) # Add IDs
+#   pks.points <- ggplot2::fortify(pks.pienalue, region="id") # Get point data
+#   
+#   pks.points$group <- sub(".1", "", pks.points$group) # Regex PIEN to joinable format
+#   
+#   pks.df <- merge(pks.points, pks.pienalue@data, by.x="group", by.y = "id") # Put everything together
+#   pks.df <- pks.df[order(pks.df$order),] # sort DF so that polygons come out in the right order
+#   
+#   # Fix encoding
+# #  pks.df$Nimi <- factor(iconv(pks.df$Nimi, from="ISO-8859-1", to="UTF-8"))
+# #  pks.df$NIMI_ISO <- factor(iconv(pks.df$NIMI_ISO, from="ISO-8859-1", to="UTF-8"))
+#   pks.df$Name <- factor(iconv(pks.df$Name, from="ISO-8859-1", to="UTF-8"))
+# #  pks.pienalue@data$Nimi <- factor(iconv(pks.pienalue@data$Nimi, from="ISO-8859-1", to="UTF-8"))
+# #  pks.pienalue@data$NIMI_ISO <- factor(iconv(pks.pienalue@data$NIMI_ISO, from="ISO-8859-1", to="UTF-8"))
+#   pks.pienalue@data$Name <- factor(iconv(pks.pienalue@data$Name, from="ISO-8859-1", to="UTF-8"))
+#   
   message("DONE\n")
   return(list(pienalue=pks.pienalue, pienalue.df=pks.df))
 }
